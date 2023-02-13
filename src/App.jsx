fd51bac4d241e7e2d7cefe8097150be0aa58a566
sha1good <bankoleyakub11@yahoo.com>
2023-02-13 18:09:02 +0100

    Netflix Clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/list/List.jsx
@@ -0,0 +1,59 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import ListItem from "../listItem/ListItem";
+import { useRef, useState } from "react";
+import "./list.scss";
+
+const List = () => {
+  const listRef = useRef();
+  const [sliderNumber, setSliderNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction) => {
+     setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+
+    if (direction === "left" && sliderNumber > 0) {
+      setSliderNumber(sliderNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && sliderNumber < 5) {
+      setSliderNumber(sliderNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+
+  //  style={{ opacity: !isMoved && "0"}}
+  return (
+    <div className="list">
+      <span className="listTitle"> Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none"}}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem index={0}/>
+          <ListItem index={1}/>
+          <ListItem index={2}/>
+          <ListItem index={3}/>
+          <ListItem index={4}/>
+          <ListItem index={5}/>
+          <ListItem index={6}/>
+          <ListItem index={7}/>
+          <ListItem index={8}/>
+          <ListItem index={9}/>
+        </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default List;
